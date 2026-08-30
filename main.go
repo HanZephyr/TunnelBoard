3b4dc54f6f3e0d2b58b059d116ffffb3d57ac7fa
HanZephyr <HanZephyr@outlook.com>
2026-08-30 22:56:37 +0800

    chore(release): bump version to 1.0.12

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ var trayIconFallback []byte
 const (
 	applicationTitle = "TunnelBoard"
 	singleInstanceID = "tunnelboard-single-instance"
-	appVersion       = "1.0.11"
+	appVersion       = "1.0.12"
 )
 
 func main() {
